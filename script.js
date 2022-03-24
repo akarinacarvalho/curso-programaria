ce0a145fc7673179dabd13852e199224cedcd3b9
akarinacarvalho <karinacarvalho@live.com>
2022-03-23 23:04:11 -0300

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-if(document.getElementById("nome")&&){
-
+if(document.getElementById("nome") estiver preenchido && document.getElementById("email") estiver preenchido){
+  envie os dados
 }else{
-
+  peça a usuária que preencha os campos nome e email
 }
