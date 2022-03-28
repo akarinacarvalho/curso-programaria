89b5654d92d599eee966e839e7ba16fc49e06ead
akarinacarvalho <karinacarvalho@live.com>
2022-03-27 23:06:52 -0300

    update

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-if(document.getElementById("nome").value != "" && document.getElementById("email").value != ""){
-  alert("Prontinho! Você receberá as novidades por email.")
-}else{
-  alert("Por favor, preencha os campos nome e email!")
-}
+function validaFormulario(){
+
+  if(document.getElementById("nome").value != "" &&
+      document.getElementById("email").value != "" &&
+      document.getElementById("telefone").value != "" ){
+
+    alert("Prontinho! você receberá as novidades por email.")
+   }else{
+     alert("Por favor, preencha os campos nome e email.")
+   }
+ }
